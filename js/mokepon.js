32deb638502f7bd3909b27451df6fa9750a2737b
JulianYR7 <juliandavidyr15@gmail.com>
2025-02-05 21:13:53 -0500

    Actualizacion del DOM en HTML

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,28 +1,94 @@
-// Variables
+// Variables Globales
 
-function iniciarJuego() {
+let ataqueJugador;
+let ataqueEnemigo;
+
+let btnFuegoClick = false;
+let btnAguaClick = false;
+let btnTierraClick = false;
+
+function iniciarJuego(){ // Inicio del juego y variables principales
     let botonMascotaJugador = document.getElementById('boton-mascota');
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+
+    const btnFuego = document.getElementById('botonFuego').addEventListener('click', ataque, () => {
+        btnFuegoClick = true;
+    });
+    const btnAgua = document.getElementById('botonAgua').addEventListener('click', ataque, () => {
+        btnAguaClick = true;
+    });
+    const btnTierra = document.getElementById('botonTierra').addEventListener('click', ataque, () => {
+        btnTierraClick = true;
+    });
+
 }
 
-function seleccionarMascotaJugador() {
+// Selección de personajes
+function seleccionarMascotaJugador(){
     const inputHipodoge = document.getElementById('hipodoge');
     const inputCapipepo = document.getElementById('capipepo');
     const inputRatigueya = document.getElementById('ratigueya');
 
+    const spanMascotaJugador = document.getElementById('mascotaJugador');
+
     if (inputHipodoge.checked){
-        alert("Hipodoge listo para pelear.");
+        alert("Soldado Hipodoge listo para pelear, señor.");
+        spanMascotaJugador.innerHTML = 'Hipodoge';
+        seleccionarMascotaEnemigo();
     }
     else if (inputCapipepo.checked){
         alert("Oh sii, es mi turno.");
+        spanMascotaJugador.innerHTML = 'Capipepo';
+        seleccionarMascotaEnemigo();
     }
     else if (inputRatigueya.checked){
-        alert("Preparate, por que voy con todo.")
+        alert("Que se preparen, por que voy con todo.")
+        spanMascotaJugador.innerHTML = 'Ratigueya';
+        seleccionarMascotaEnemigo();
     }
     else {
         alert("Jugar solo es aburrido. Elige una mascota.")
         return ;
     }
+}
+
+function seleccionarMascotaEnemigo(){
+
+    const mascotaEnemigo = aleatorio(1,3)
+    const spanMascotaEnemigo = document.getElementById('mascotaEnemigo');
+
+    if (mascotaEnemigo == 1){
+        spanMascotaEnemigo.innerHTML = 'Hipodoge';
+    }
+    else if (mascotaEnemigo == 2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo';
+    }
+    else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya';
+    }
+}
+
+function ataque(){
+
+    if (btnFuegoClick === true){
+        ataqueJugador = "Fuego";
+        alert('Funciona!!!!!!')
+    }
+    else if (btnAguaClick === true){
+        ataqueJugador = "Agua";
+        alert('Funciona!!!!!!')
+    }
+    else {
+        ataqueJugador = "Tierra";
+        alert('Funciona!!!!!!')
+    }
+
+}
+
+function aleatorio(min, max){ // Función aleatoria para casos del rival
+    
+    Math.floor(Math.random() * (max - min + 1) - min);
+
 }
 
 window.addEventListener('load', iniciarJuego);
